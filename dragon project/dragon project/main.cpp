14725bbad39071121af7d78db6bd2da6c4a5f99e
ephraim <ephraimjonah2017@gmail.com>
2019-07-30 07:03:51 +0100

    created a window

--- dragon project/dragon project/main.cpp
+++ dragon project/dragon project/main.cpp
@@ -28,5 +28,16 @@ int main(int argc,char* argv[])
 	{
 		std::cout << IMG_GetError << std::endl;
 	}
+
+	SDL_Window* window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 720, 480, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
 	return 0;
 }
