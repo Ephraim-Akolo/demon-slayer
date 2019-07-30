7902c557e7f69987bf14bb1508c6484a980cbec3
ephraim <ephraimjonah2017@gmail.com>
2019-07-30 05:53:00 +0100

    created a programmable sdl environment

---
+++ dragon project/dragon project/main.cpp
@@ -0,0 +1,28 @@
+// dragon project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+
+
+int main(int argc,char* argv[])
+{
+    std::cout << "Hello World!\n"; 
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << SDL_GetError() << std::endl;
+	}
+	if (TTF_Init()<0)
+	{
+		std::cout << TTF_GetError << std::endl;
+	}
+	if (Mix_Init(MIX_INIT_MP3)<0)
+	{
+		std::cout << Mix_GetError << std::endl;
+	}
+	return 0;
+}
